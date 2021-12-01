03cbd2cb9b5e6c3d595f0446f05040fce7e31daa
Nihal Rao <nihalrao@Nihals-Air.hsd1.ma.comcast.net>
2021-12-01 18:10:10 -0400

    edited medication

--- src/Business/Medication/Prescription.java
+++ src/Business/Medication/Prescription.java
@@ -12,6 +12,21 @@ import java.util.ArrayList;
  * @author revanthkatha
  */
 public class Prescription {
-    private ArrayList<Drug> prescription;
+    private ArrayList<Drug> drugslist;
     
+    public Prescription () {
+        drugslist = new ArrayList<>();
+    }
+    
+    public ArrayList<Drug> getList() {
+        return drugslist;
+    }
+    
+    public void setList(ArrayList<Drug> dl) {
+        drugslist = dl;
+    }
+    
+    public void addDrug(Drug d) {
+        drugslist.add(d);
+    }
 }
